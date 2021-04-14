903c040f59a707db66f08ac3d55bb2205df8ebe3
oliviaf <olivia.flynn2@hotmail.com>
2021-04-14 20:52:10 +0100

    v1 of mp model

--- mp/models.py
+++ mp/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
 class Mp(models.Model):
-    name = models.CharField(max_length=80)
-    location = models.CharField(max_length=80)
-    image = models.CharField(max_length=100)
-    free_school_meals = models.CharField(max_length=20)
-    coronavirus_face_coverings = models.CharField(max_length=20)
-    testing_of_nhs_staff = models.CharField(max_length=20)
-    police_crime_sentencing = models.CharField(max_length=20)
-    immigration_and_social_security = models.CharField(max_length=20)
-    environment_bill = models.CharField(max_length=20)
-    european_union_withdrawal = models.CharField(max_length=20)
-    domestic_abuse_bill = models.CharField(max_length=20)
-    nhs_pay_award = models.CharField(max_length=20)
-    agriculture_act = models.CharField(max_length=20)
+    name = models.CharField(max_length=80, default='name')
+    location = models.CharField(max_length=80, default='location')
+    image = models.CharField(max_length=100, default='img')
+    free_school_meals = models.CharField(max_length=20, default='yes')
+    coronavirus_face_coverings = models.CharField(max_length=20, default='yes')
+    testing_of_nhs_staff = models.CharField(max_length=20, default='yes')
+    police_crime_sentencing = models.CharField(max_length=20, default='yes')
+    immigration_and_social_security = models.CharField(max_length=20, default='yes')
+    environment_bill = models.CharField(max_length=20, default='yes')
+    european_union_withdrawal = models.CharField(max_length=20, default='yes')
+    domestic_abuse_bill = models.CharField(max_length=20, default='yes')
+    nhs_pay_award = models.CharField(max_length=20, default='yes')
+    agriculture = models.CharField(max_length=20, default='yes')
+    offensive_weapons_report_nc26 = models.CharField(max_length=20, default='yes')
+    windrush = models.CharField(max_length=20, default='yes')
+    unaccompanied_children = models.CharField(max_length=20, default='yes')
+    trade_bill = models.CharField(max_length=20, default='yes')
+    financial_assitance_to_industry = models.CharField(max_length=20, default='yes')
+    health_protection = models.CharField(max_length=20, default='yes')
+    brexit_air_quality = models.CharField(max_length=20, default='yes')
+    combating_violence_against_women = models.CharField(max_length=20, default='yes')
+    relationships_sex_education = models.CharField(max_length=20, default='yes')
+    brexit_rural_economy = models.CharField(max_length=20, default='yes')
+
+
 
 
 
