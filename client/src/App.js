cfdb73e64eadf35e4d5c3bcdb953b562bdaa4ef8
oliviaf <olivia.flynn2@hotmail.com>
2021-04-15 20:03:14 +0100

    more routing and set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import MpShow from './components/MpShow'
+import MpIndex from './components/MpIndex'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
@@ -11,7 +12,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/mps" >
+          <Route exact path="/mps/:id" >
             <MpShow />
           </Route>
           <Route exact path="/login">
@@ -19,6 +20,9 @@ const App = () => {
           </Route>
           <Route exact path="/register">
             <Register />
+          </Route>
+          <Route exact path="mps">
+            <MpIndex />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ client/src/components/MpIndex.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MpIndex = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MpIndex
