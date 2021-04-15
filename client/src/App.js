08e9141d7c14ad2e70947e92c6782b57f9ded235
oliviaf <olivia.flynn2@hotmail.com>
2021-04-15 19:59:14 +0100

    initial routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import MpShow from './components/MpShow'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 const App = () => {
-  const [mp, setMp] = useState('')
-  console.log(mp, setMp)
-
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get('api/mp/')
-      setMp(response.data)
-    }
-    getData()
-  }, [])
-
 
   return (
-    <div className="columns">
-      <div className="column is-one-quarter-desktop is-one-third-tablet">
-
-        {mp &&
-          <div >
-            {mp.map(mp => (
-
-              <div className="card" key={mp.name}>
-                <div className="card-header">
-                  <div key={mp.id} className="card-header-title">{mp.location}</div>
-                </div>
-
-
-              </div>
-            ))}
-          </div>
-        }
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/mps" >
+            <MpShow />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
 
   )
 }
---
+++ client/src/components/MpShow.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+const MpShow = () => {
+  const [mp, setMp] = useState('')
+  console.log(mp, setMp)
+
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('api/mp/')
+      setMp(response.data)
+    }
+    getData()
+  }, [])
+
+
+  return (
+    <div className="columns">
+      <div className="column is-one-quarter-desktop is-one-third-tablet">
+
+        {mp &&
+          <div >
+            {mp.map(mp => (
+
+              <div className="card" key={mp.name}>
+                <div className="card-header">
+                  <div key={mp.id} className="card-header-title">{mp.location}</div>
+                </div>
+
+
+              </div>
+            ))}
+          </div>
+        }
+      </div>
+    </div>
+
+  )
+}
+
+export default MpShow
