81a5e3e2b818e21bd9ac5c3b5acfa41ce3237f1a
oliviaf <olivia.flynn2@hotmail.com>
2021-04-18 15:49:53 +0100

    profile get request working

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,15 +1,25 @@
 /*eslint-disable no-unused-vars*/
 
 import React, { useEffect, useState } from 'react'
-import { getPayloadFromToken, userIsOwner } from '../components/helpers/auth'
+import { getPayloadFromToken } from '../components/helpers/auth'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 import Navbar from '../components/Navbar'
 
 const Profile = () => {
   const [user, setUser] = useState(null)
+  console.log(user, setUser)
 
-  const userID = getPayloadFromToken
+  const userID = getPayloadFromToken().sub
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`/api/auth/${userID}/`)
+      setUser(response.data)
+    }
+    getData()
+  }, [])
 
 
 
